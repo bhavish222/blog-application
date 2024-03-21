7abbc72defe786230e6988536580d3155b5dbbf2
bhavish222 <bhavish0970@chitkara.edu.in>
2024-03-21 18:14:54 +0530

    modified postServiceImplementation

--- src/main/java/io/mountblue/BlogApplication/services/PostServiceImplementation.java
+++ src/main/java/io/mountblue/BlogApplication/services/PostServiceImplementation.java
@@ -16,11 +16,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
-public class PostServiceImplementation implements PostService{
+public class PostServiceImplementation implements PostService {
 
     public PostServiceImplementation() {
 
     }
+
     private PostRepository postRepository;
     private TagRepository tagRepository;
 
@@ -35,7 +36,7 @@ public class PostServiceImplementation implements PostService{
 
     @Override
     public Page<Post> paginationPage(int pageNo, int pageSize) {
-        Pageable pageable = PageRequest.of(pageNo-1, pageSize);
+        Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
         return postRepository.findAll(pageable);
     }
 
@@ -43,32 +44,38 @@ public class PostServiceImplementation implements PostService{
     public void save(Post post) {
         postRepository.save(post);
     }
+
     @Override
     public List<Post> findAllPosts() {
         return postRepository.findAll();
     }
+
     @Override
-    public Post findPostById(Long id){
+    public Post findPostById(Long id) {
         return postRepository.findPostById(id);
     }
+
     @Override
     public void deletePostById(Long id) {
         postRepository.deleteById(id);
     }
+
     @Override
     public List<Post> getPostsSortedByDate(List<Post> posts) {
         return postRepository.findPostsInAndOrderByPublishedAtDesc(posts);
     }
+
     @Override
     public List<Post> getPostsSortedByOldestDate(List<Post> posts) {
         return postRepository.findPostsInAndOrderByPublishedAtAsc(posts);
     }
+
     @Override
     public String findTagsOfPostToString(Post post) {
         List<Tag> listOfTags = post.getTags();
         StringBuilder tagListBuilder = new StringBuilder();
         if (listOfTags != null) {
-            for(Tag tag : listOfTags) {
+            for (Tag tag : listOfTags) {
                 tagListBuilder.append(tag.getName()).append(",");
             }
         }
@@ -78,7 +85,7 @@ public class PostServiceImplementation implements PostService{
     @Override
     public void saveOrUpdate(Post post, String tagsString, String action) {
         String[] tagNames = tagsString.split(",");
-        List<Tag> newTags= new ArrayList<>();
+        List<Tag> newTags = new ArrayList<>();
         for (String tagName : tagNames) {
             Tag tag = new Tag();
             tag.setName(tagName.trim());
@@ -86,18 +93,18 @@ public class PostServiceImplementation implements PostService{
         }
         List<Tag> existingTags = tagRepository.findAll();
         List<Tag> allUniqueTagsForThisPost = new ArrayList<>();
-        for(Tag tag : newTags) {
+        for (Tag tag : newTags) {
             String tagName = tag.getName();
             boolean checkIfExists = false;
-            for(Tag tempTag : existingTags) {
+            for (Tag tempTag : existingTags) {
                 String tempTagName = tempTag.getName();
-                if(tempTagName.equals(tagName)) {
+                if (tempTagName.equals(tagName)) {
                     checkIfExists = true;
                     allUniqueTagsForThisPost.add(tempTag);
                     break;
                 }
             }
-            if(!checkIfExists) {
+            if (!checkIfExists) {
                 allUniqueTagsForThisPost.add(tag);
             }
         }
@@ -105,18 +112,18 @@ public class PostServiceImplementation implements PostService{
         int currentPostLength = post.getContent().length();
         String excerpt = post.getContent().substring(0, Math.min(currentPostLength, 150));
 
-        if(action.equals("Publish")) {
+        if (action.equals("Publish")) {
             post.setCreatedAt(LocalDateTime.now());
             post.setPublishedAt(LocalDateTime.now());
             post.setIs_published(true);
         }
-        User user = new User(1L, "bhavi", "wadhwabhavish46@gmail.com","3001");
+        User user = new User(1L, "bhavi", "wadhwabhavish46@gmail.com", "3001");
         post.setAuthor(user);
         post.setExcerpt(excerpt);
         post.setUpdatedAt(LocalDateTime.now());
-        if(post.getId() != null) {
+        if (post.getId() != null) {
             Post existingPost = findPostById(post.getId());
-            if(existingPost != null) {
+            if (existingPost != null) {
                 existingPost.setTitle(post.getTitle());
                 existingPost.setContent(post.getContent());
                 existingPost.setTags(post.getTags());
